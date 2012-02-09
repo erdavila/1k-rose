2d5a6fb97b5a6041057d421b39a03e6c086c725e
Eduardo R. D'Avila <erdavila@gmail.com>
2012-02-09 18:50:51 -0200

    Including some explanation for the colors.

--- 1k-rose.reverse-engineered.js
+++ 1k-rose.reverse-engineered.js
@@ -12,8 +12,8 @@ function surface(a, b, c) {
 			x: Math.sin(a * 7) * (13 + 5 / (.2 + Math.pow(b * 4, 4))) - Math.sin(b) * 50,
 			y: b * SIZE + 50,
 			z: 625 + Math.cos(a * 7) * (13 + 5 / (.2 + Math.pow(b * 4, 4))) + b * 400,
-			3: a * 1 - b / 2,
-			4: a
+			r: a * 1 - b / 2,
+			g: a
 		};
 	}
 	
@@ -44,10 +44,11 @@ function surface(a, b, c) {
 				x: o * Math.cos(n) + w * Math.sin(n) + j * 610 - 390,
 				y: o * Math.sin(n) - w * Math.cos(n) + 550 - j * 350,
 				z: 1180 + Math.cos(B + A) * 99 - j * 300,
-				3: .4 - a * .1 + Math.pow(1 - B * B, -h * 6) * .15 - a * b * .4 + Math.cos(a + b) / 5 + Math.pow(Math.cos((o * (a + 1) + (B > 0 ? w : -w)) / 25), 30) * .1 * (1 - B * B),
-				4: o / 1e3 + .7 - o * w * 3e-6
+				r: .4 - a * .1 + Math.pow(1 - B * B, -h * 6) * .15 - a * b * .4 + Math.cos(a + b) / 5 + Math.pow(Math.cos((o * (a + 1) + (B > 0 ? w : -w)) / 25), 30) * .1 * (1 - B * B),
+				g: o / 1e3 + .7 - o * w * 3e-6
 			};
 		}
+		
 		if(c > 32) {
 			// The 4 SEPALS.
 			// There are 4 values for which 32 < c <= 37, one for each sepal.
@@ -59,8 +60,8 @@ function surface(a, b, c) {
 				x: o * Math.cos(c) - w * Math.sin(c),
 				y: 28 + Math.cos(B * .5) * 99 - b * b * b * 60 - z / 2 - h,
 				z: z,
-				3: (b * b * .3 + Math.pow((1 - (A * A)), 7) * .15 + .3) * b,
-				4: b * .7
+				r: (b * b * .3 + Math.pow((1 - (A * A)), 7) * .15 + .3) * b,
+				g: b * .7
 			};
 		}
 		
@@ -73,8 +74,8 @@ function surface(a, b, c) {
 			x: o * Math.cos(c) - w * Math.sin(c),
 			y: B * 99 - Math.cos(Math.pow(b, 7)) * 50 - c / 3 - z / 1.35 + 450,
 			z: z,
-			3: (1 - b / 1.2) * .9 + a * .1,
-			4: Math.pow((1 - b), 20) / 4 + .05
+			r: (1 - b / 1.2) * .9 + a * .1,
+			g: Math.pow((1 - b), 20) / 4 + .05
 		};
 	}
 }
@@ -95,9 +96,17 @@ setInterval(function () {
 			var zBufferIndex = y * SIZE + x;
 			if((typeof zBuffer[zBufferIndex] === "undefined")  ||  (zBuffer[zBufferIndex] > z)) {
 				zBuffer[zBufferIndex] = z;
-				var r = -parseInt(point[3] * h);
-				var g = -parseInt(point[4] * h);
-				var b = -parseInt(point[3] * point[3] * -80);
+				/*
+				The rose is mostly red and green - that's why the objects
+				returned by the surface function do not have a field for the
+				blue component. However, a bit of the blue component is desired
+				to "tune" the colors of the petals, so the blue component can be
+				derived from the red component.
+				 */
+				var r = -parseInt(point.r * h);
+				var g = -parseInt(point.g * h);
+				var b = -parseInt(point.r * point.r * -80);
+				
 				context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
 				context.fillRect(x, y, 1, 1);
 			}
